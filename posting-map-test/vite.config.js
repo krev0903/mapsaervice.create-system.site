921cc148b2243790d744e2ca167caaec7edbdeb4
kurebayashi <ta09sho03ko77n@gmail.com>
2025-04-19 23:54:35 +0900

    help2

--- posting-map-test/vite.config.js
+++ posting-map-test/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         }),
         react(),
     ],
-    base: '/posting-map-test/build/assets',
+    base: '/build/assets',
     build: {
         outDir: 'public/build/assets',
         manifest: true,
