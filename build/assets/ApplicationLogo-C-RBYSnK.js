627dae3927a1b6f988459270c3286fde73662f21
kurebayashi <ta09sho03ko77n@gmail.com>
2025-05-05 22:00:02 +0900

    test

--- build/assets/ApplicationLogo-C-RBYSnK.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./app-C_In_P_d.js";function p(o){return i.jsx("img",{...o,src:"/image/logo.jpg",alt:"Application Logo"})}export{p as A};
--- build/assets/InputLabel-DAyKfeuq.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-C_In_P_d.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
